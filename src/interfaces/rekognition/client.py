58adbc742566dbfd512b0dbb87710b01c8206668
Ranjani Krishnan <ranjanikrishnanr@gmail.com>
2019-11-08 17:26:36 +0100

    adjusted the response from rekognition to required format

--- src/interfaces/rekognition/client.py
+++ src/interfaces/rekognition/client.py
@@ -2,10 +2,7 @@ import os
 import boto3
 
 def get_connection():
-	# aws_access='AKIAT4T4WB4ZFNXOY7PE'
-	# aws_secret='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'
-	
-	aws_access = str(os.environ['AWS_ACCESS'])
-	aws_secret= str(os.environ['AWS_SECRET'])
+	aws_access = os.environ['AWS_ACCESS'].strip('\'')
+	aws_secret=os.environ['AWS_SECRET'].strip('\'')
 	rekognition = boto3.client("rekognition", region_name='eu-west-1', aws_access_key_id=aws_access, aws_secret_access_key=aws_secret)
 	return rekognition
--- src/interfaces/rekognition/rekognition.py
+++ src/interfaces/rekognition/rekognition.py
@@ -2,12 +2,12 @@ import os
 import boto3
 from interfaces.rekognition import client
 
-
 def compare_faces(source_bytes, target_bytes, threshold=80):
 	
 	rekognition = client.get_connection()
 	response = rekognition.compare_faces(SourceImage={'Bytes': source_bytes},
 		                                 TargetImage={'Bytes': target_bytes},
 	                                     SimilarityThreshold=threshold)
-	probability = response['FaceMatches'][0]['Similarity']
+	probability_response = response['FaceMatches'][0]['Similarity']/100
+	probability = round(probability_response,2)
 	return probability
